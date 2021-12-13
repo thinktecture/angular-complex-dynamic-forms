b5bf6ccb8130d3302e69f0d5dfed673f473a1b56
Max Schulte <max.schulte@thinktecture.com>
2021-12-13 17:22:36 +0100

    fix for stackblitz

--- projects/forms-test/src/app/person-details/person-details.component.ts
+++ projects/forms-test/src/app/person-details/person-details.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'tt-person-details',
     templateUrl: './person-details.component.html',
     styleUrls: ['./person-details.component.scss'],
-    viewProviders: [{ provide: FormGroupDirective, useExisting: FormGroupDirective }],
 })
 export class PersonDetailsComponent {
     @Input() formGroup!: FormGroup;
